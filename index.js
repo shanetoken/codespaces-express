a2e98b8409c0475a3a07cbd65055492617491254
shanetoken <158006619+shanetoken@users.noreply.github.com>
2024-03-05 10:40:10 +0000

    Video1 - Blog - NetNinja

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
+require('dotenv').config()
+
 const express = require('express')
 const app = express()
+
+const expressLayout = require('express-ejs-layouts')
 const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.use(express.static('public'))
+app.use('/', require('./server/routes/main'))
+
+// Templating
+app.use(expressLayout)
+app.set('layout', './layouts/main')
+app.set('view engine', 'ejs')
+
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
